46e7120fbb7028fcd5a8b818d06076f5678a9733
masonseiler <masonseiler1@gmail.com>
2018-07-03 14:41:37 -0400

    case change

--- coffeeclient/src/helpers/environment.js
+++ coffeeclient/src/helpers/environment.js
@@ -4,5 +4,7 @@ switch(window.location.hostname) {
     case 'localhost' :
         APIURL = 'http://localhost:3000';
         break;
+    case 'mrseiler-coffeeapi.herokuapp.com' :
+        APIRUL = 'https://mrseiler-coffeeapi.herokuapp.com'
 }
 export default APIURL;
